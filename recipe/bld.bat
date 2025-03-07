6de81a8f2cdbdd868ba31e2e8f06d311facf1015
Filipe Fernandes <ocefpaf@gmail.com>
2025-03-07 14:35:48 -0300

    noarch patch

--- recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-set LIB=%LIBRARY_LIB%;%LIB%
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
-%PYTHON% -m pip install --no-deps --ignore-installed .
-if errorlevel 1 exit 1
